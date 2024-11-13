166a80eec3c5a873d64075556e285a578dee1fa7
Nathon Teachasrisutee <jommy346@gmail.com>
2024-11-13 08:04:50 +0700

    add celery task file and update celery settings

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -1,11 +1,14 @@
+from __future__ import absolute_import, unicode_literals
 import os
+
 
 from celery import Celery
 
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
-app = Celery('proj')
+# Set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+app = Celery('celery')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'celery',
     'rest_framework',
     'corsheaders',
 ]
